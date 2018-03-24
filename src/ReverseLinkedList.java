d2090476c924c72e4eedfca25c7d5a486e7883c5
Robert Hunter <roberthunter@Roberts-MBP.lan1>
2018-03-24 01:52:30 -0500

    Completed Reverse LinkedList implementation

--- src/ReverseLinkedList.java
+++ src/ReverseLinkedList.java
@@ -1,2 +1,28 @@
+import java.util.LinkedList;
+
 public class ReverseLinkedList {
+    public static void main (String[] args) {
+        System.out.println("Hello");
+        System.out.println("Original List"+generateLinkedList());
+        System.out.println("Reversed List"+reverseLinkedList(generateLinkedList()));
+    }
+
+    public static LinkedList<Integer> generateLinkedList() {
+        LinkedList<Integer> object = new LinkedList<Integer>();
+        object.add(10);
+        object.add(30);
+        object.add(2);
+        object.add(3);
+        object.add(63);
+        return object;
+    }
+
+    public static LinkedList<Integer> reverseLinkedList(LinkedList<Integer> originalList){
+        int listLength = originalList.size();
+        LinkedList<Integer> newList = new LinkedList<Integer>();
+        for(int i = 0; i < listLength; i++){
+            newList.add(originalList.removeLast());
+        }
+        return newList;
+    }
 }
